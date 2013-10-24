ab0c4f2a374c9e4d488cb2ebc38d22a0d39f21bb
josh <josh.ribakoff@gmail.com>
2013-10-23 22:43:15 -0400

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync --progress -k --exclude='/cache' --exclude='.git' --checksum -rv . root@metator.com:/var/www/prestashop.vehiclefits.com/html
+rsync --progress -Lvr --exclude='.git' --checksum --delete-after . root@198.58.123.231:/var/www/prestashop.vehiclefits.com
